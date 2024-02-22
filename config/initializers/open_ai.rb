bdb7191e528b05f3b76a1b271024bb5e7a8f76dc
OmarMWarraich <earthling986@gmail.com>
2024-02-22 17:02:24 +0500

    feat: Turbo Stream

--- config/initializers/open_ai.rb
+++ config/initializers/open_ai.rb
@@ -1,4 +1,4 @@
-OpenAi.configure do |config|
-    config.access_token = ENV['OPENAI_ACCESS_TOKEN']
-    config.request_timeout = 240
-end
+# OpenAi.configure do |config|
+#     config.access_token = ENV['OPENAI_ACCESS_TOKEN']
+#     config.request_timeout = 240
+# end

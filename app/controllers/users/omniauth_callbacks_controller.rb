3d015d87cd675ad54ce9042ad25c7bf3054f5818
OmarMWarraich <earthling986@gmail.com>
2024-02-24 20:11:50 +0500

    feat: Google Integration, Button Google

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -15,9 +15,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       sign_out_all_scopes
       flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
       sign_in_and_redirect user, event: :authentication
-
     else
-      flash[:alert] = t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: 'authentication error'
+      flash[:alert] =
+        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
       redirect_to new_user_session_path
     end
   end
@@ -45,6 +45,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   private
 
   def auth
-    @auth || = request.env['omniauth.auth']
+    @auth ||= request.env['omniauth.auth']
   end
 end
